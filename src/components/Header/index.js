1e2a869423e5eace001ff922fcb972ab87e343e0
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2022-02-05 12:02:18 +0700

    slicing header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,47 @@ import styles from "./styles.module.css";
 export default () => {
     return (
         <div className={styles.container}>
-            <div>FAB</div>
+            <img
+                src="profile_pict.jpeg"
+                alt="profile pict"
+                className={styles.imageProfile}
+            />
+            <div className={styles.containerText}>
+                <div className={styles.itemTextIconUserData}>
+                    <span className={styles.textName}>Vito Rizki Imanda</span>
+                    <img
+                        src="icon_verified.svg"
+                        alt="profile pict"
+                        className={styles.imageIconVerified}
+                    />
+                    <img
+                        src="icon_hamburger.svg"
+                        alt="profile pict"
+                        className={styles.imageIconHamburger}
+                    />
+                </div>
+                <div className={styles.textDesc500}>Front-end Engineer</div>
+                <div className={styles.containerTextIcon}>
+                    <span className={styles.itemTextIcon}>
+                        <img
+                            src="icon_pinpoint.svg"
+                            alt="profile pict"
+                            className={styles.imageIcon}
+                        />
+                        <span className={styles.textDesc}>Jakarta</span>
+                    </span>
+                    <span className={styles.itemTextIcon}>
+                        <img
+                            src="icon_email.svg"
+                            alt="profile pict"
+                            className={styles.imageIcon}
+                        />
+                        <span className={styles.textDesc}>
+                            vitorizkiimanda@gmail.com
+                        </span>
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Listing/index.js
+++ src/components/Listing/index.js
@@ -3,11 +3,19 @@ import styles from "./styles.module.css";
 const Listing = (props) => {
     const { dataList } = props;
     return (
-        <div className={styles.container}>
-            {dataList.map((val, index) => (
-                <img src={val.url} alt="" className={styles.image} />
-            ))}
-        </div>
+        <>
+            <div className={styles.container}>
+                {dataList.map((val, index) => (
+                    <img
+                        src={val.url}
+                        alt=""
+                        className={styles.image}
+                        key={val.id}
+                    />
+                ))}
+            </div>
+            <div className={styles.bottomTransition} />
+        </>
     );
 };
 
